a2aa054399b7373f6157e90a2755b69fa362484c
MounikaChekuri94 <126592485+MounikaChekuri94@users.noreply.github.com>
2023-03-20 10:14:29 +0530

    Add files via upload

---
+++ Python/Activities/Practice1.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+	
+myVariable = 5
+print(myVariable) # Output: 5
+myVariable = 9
+print(myVariable)
+print("Hello, World!") # This is a comment 
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+print("Hello")
+print('Hello') 
